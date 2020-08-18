4711fbf5aca5726a755203b1964cbd8c252108b4
bga <bga.email@gmail.com>
2020-08-18 13:18:08 +0300

    replace long char literals to macro `makeU32leFrom4U8`

--- include/!mcu/UartProcessor.h
+++ include/!mcu/UartProcessor.h
@@ -15,12 +15,12 @@
 	#include <!cpp/debug.h>
 #endif // TESTRUNNER_ON
 
-//namespace UartProcessor { 
+//namespace UartProcessor {
 #if 0
 struct Config {
 	typedef FU32 ErrorCode;
 	typedef FU16 Name;
-	
+
 	enum {
 		nameMaxSize = sizeof(Name),
 		operatorSize = 1,
@@ -29,19 +29,19 @@ struct Config {
 		bufferSize = nameMaxSize + operatorSize + dataMaxSize
 	};
 	void write(FU8 byte) {
-		
+
 	}
 };
 #endif // 1
 //# write { p00=FF00\n } read { p00?\n }
-template<typename ConfigArg> 
+template<typename ConfigArg>
 struct UartProcessor {
 	typedef ConfigArg Config;
 	Config config;
-	
+
 	typedef typename Config::ErrorCode ErrorCode;
 	typedef typename Config::Name Name;
-	
+
 	virtual ErrorCode readData(FU16 name, MemoryRegion& data) = 0; union {
 
 	};
@@ -50,29 +50,29 @@ struct UartProcessor {
 	};
 
 	enum {
-		Error_ok = staticSwapBytes32('OK\x00\x00'), 
-		Error_bufferOverflow = staticSwapBytes32('#OVF'), 
-		Error_nameNotFound = staticSwapBytes32('#NAM'), 
-//		Error_valueSize = staticSwapBytes32('#V_S'), 
-//		Error_valueData = staticSwapBytes32('#V_D'), 
-		Error_valueSize = staticSwapBytes32('#VSI'), 
-		Error_valueData = staticSwapBytes32('#VDA'), 
-		Error_unknownOperator = staticSwapBytes32('#OP\x00'), 
-		Error_unknown = staticSwapBytes32('#UNK'), 
-//		Error_ = '#', 
+		Error_ok = staticSwapBytes32(makeU32leFrom4U8('O', 'K', '\x00','\x00')),
+		Error_bufferOverflow = staticSwapBytes32(makeU32leFrom4U8('#', 'O', 'V', 'F')),
+		Error_nameNotFound = staticSwapBytes32(makeU32leFrom4U8('#', 'N', 'A', 'M')),
+//		Error_valueSize = staticSwapBytes32(makeU32leFrom4U8('#', 'V', '_', 'S')),
+//		Error_valueData = staticSwapBytes32(makeU32leFrom4U8('#', 'V', '_', 'D')),
+		Error_valueSize = staticSwapBytes32(makeU32leFrom4U8('#', 'V', 'S', 'I')),
+		Error_valueData = staticSwapBytes32(makeU32leFrom4U8('#', 'V', 'D', 'A')),
+		Error_unknownOperator = staticSwapBytes32(makeU32leFrom4U8('#', 'O', 'P', '\x00')),
+		Error_unknown = staticSwapBytes32(makeU32leFrom4U8('#', 'U', 'N', 'K')),
+//		Error_ = '#',
 	};
-	
+
 	enum {
 		bufferSize = Config::bufferSize
 	};
 
 	U8 buffer[bufferSize];
 	FU8 bufferPos;
-	
+
 	void init() {
 		this->bufferPos = 0;
 	}
-	
+
 	static FU8 nibbleToHex(FU8 x) {
 		if(x < 10) {
 			return '0' + x;
@@ -128,7 +128,7 @@ struct UartProcessor {
 		}
 		#endif // 0
 	}
-	
+
 	void read(FU8 ch) {
 		if(bufferSize <= this->bufferPos) {
 			this->reportError(Error_bufferOverflow);
@@ -214,7 +214,7 @@ struct UartProcessor {
 			else {
 				errorCode = Error_unknownOperator;
 			}
-			
+
 			this->bufferPos = 0;
 			this->reportError(errorCode);
 		}
@@ -229,16 +229,16 @@ example("UartProcessor") {
 //			size_t startpos = str.find_first_not_of(" \t");
 			return ((std::string::npos != endpos) ? str.substr(0, endpos + 1) : str);
 		}
-		
+
 		U16 swapBytes(U16 x) {
 			return (U8(x) << 8) | U8(x >> 8);
 		}
 	} _;
-	
+
 	struct UartProcessorConfig {
 		typedef FU32 ErrorCode;
 		typedef FU16 Name;
-		
+
 		enum {
 			nameMaxSize = sizeof(Name),
 			operatorSize = 1,
@@ -246,9 +246,9 @@ example("UartProcessor") {
 
 			bufferSize = nameMaxSize + operatorSize + dataMaxSize
 		};
-		
+
 		std::string txBuffer;
-		
+
 		void write(FU8 byte) {
 			txBuffer.push_back(byte);
 		}
@@ -261,7 +261,7 @@ example("UartProcessor") {
 		U16 bParam;
 		ErrorCode readData(FU16 name, MemoryRegion& data) override {
 			switch(name) {
-				case('a'): return (data.data = reinterpret_cast<U8 *>(&(this->aParam))), (data.dataSize = sizeof(this->aParam)), Error_ok; 
+				case('a'): return (data.data = reinterpret_cast<U8 *>(&(this->aParam))), (data.dataSize = sizeof(this->aParam)), Error_ok;
 				default: return Error_nameNotFound;
 			}
 
@@ -269,24 +269,24 @@ example("UartProcessor") {
 		ErrorCode writeData(FU16 name, MemoryRegion data) override {
 			Debug_print("%i", data.dataSize);
 			switch(name) {
-				case('a'): return (data.dataSize == sizeof(this->aParam)) ? (this->aParam = *reinterpret_cast<U16 *>(data.data), Error_ok) : Error_valueSize; 
+				case('a'): return (data.dataSize == sizeof(this->aParam)) ? (this->aParam = *reinterpret_cast<U16 *>(data.data), Error_ok) : Error_valueSize;
 				default: return Error_nameNotFound;
 			}
 		}
-		
+
 		Bool readString(const char* cmd) {
 			while(*cmd) {
 				this->read(*cmd);
 				cmd += 1;
 			}
-			
+
 			return *cmd == 0;
 		}
 	};
-	
+
 	AppUartProcessor appUartProcessor;
 	appUartProcessor.init();
-	
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("a=ABCD\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\nOK"));
@@ -295,12 +295,12 @@ example("UartProcessor") {
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("a?\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("ABCD\nOK"));
-	
-	
+
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("a=AB\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#VSI"));
-	
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("x=AB\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#NAM"));
@@ -308,15 +308,15 @@ example("UartProcessor") {
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("x?\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#NAM"));
-	
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("x=ABA\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#VDA"));
-	
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("a$\n"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#OP"));
-	
+
 	appUartProcessor.config.txBuffer.clear();
 	assert_eq(appUartProcessor.readString("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"), yes);
 	assert_eq(_.trimRight(appUartProcessor.config.txBuffer), std::string("\n#OVF"));
