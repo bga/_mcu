42907d77476f8a1c71dd311c590d1df9f8da1331
bga <bga.email@gmail.com>
2020-10-24 18:39:45 +0300

    Added license header

--- include/!mcu/filter/median.h
+++ include/!mcu/filter/median.h
@@ -1,6 +1,21 @@
 #pragma once
 
-//# to c++ convert from [https://raw.githubusercontent.com/eddyem/fits_filter/master/median.c]
+//# to c++ convert from [https://raw.githubusercontent.com/eddyem/fits_filter/master/median.c] by Bga
+/*
+  Copyright 2020 Bga <bga.email@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 // FOR medianXX:
 // Copyright (c) 1998 Nicolas Devillard. Public domain.
